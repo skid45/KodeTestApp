e36aea866eb2fb711fed3e34cc4db04424020bd5
skid45 <akopovgrisha@gmail.com>
2023-08-12 20:03:34 +0300

    Добавлен маппер для приведения объекта сетевой модели данных UserNetworkEntity к объекту модели данных для UI: UserItem.

---
+++ data/src/main/java/com/skid/kodetestapp/data/mapper/UserMapper.kt
@@ -0,0 +1,38 @@
+package com.skid.kodetestapp.data.mapper
+
+import com.skid.kodetestapp.data.model.UserNetworkEntity
+import com.skid.kodetestapp.domain.model.UserItem
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+fun UserNetworkEntity.toUserItem(): UserItem {
+    val formatter = DateTimeFormatter.ofPattern("d MMM", Locale.forLanguageTag("ru"))
+    return UserItem(
+        id = id,
+        avatarUrl = avatarUrl,
+        name = "$firstName $lastName",
+        birthday = birthday,
+        department = department.getDisplayDepartment(),
+        userTag = userTag,
+        phone = phone,
+        monthDayOfBirthday = birthday.format(formatter).dropLastWhile { it == '.' }
+    )
+}
+
+private fun String.getDisplayDepartment(): String {
+    return when (this) {
+        "android" -> "Android"
+        "ios" -> "iOS"
+        "design" -> "Дизайн"
+        "management" -> "Менеджмент"
+        "qa" -> "QA"
+        "back_office" -> "Бэк-офис"
+        "frontend" -> "Frontend"
+        "hr" -> "HR"
+        "pr" -> "PR"
+        "backend" -> "Backend"
+        "support" -> "Техподдержка"
+        "analytics" -> "Аналитика"
+        else -> this
+    }
+}
